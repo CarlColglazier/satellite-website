f32ce92208756aefceeafa264409591baa8098ca
Carl Colglazier <carl@carlcolglazier.com>
2017-05-23 21:12:15 -0400

    Updates for 2017

---
+++ README.md
@@ -0,0 +1,4 @@
+# The SATELLITE Website
+
+This directory contains the design and functionality that makes the SATELLITE
+website tick.
--- index.php
+++ index.php
@@ -1,278 +1,108 @@
-<html>
-
-<head>
-<title>NCSU SATELLITE: Science & Technology Enriching Lifelong Leadership in Tomorrow's Endeavors</title>
-
-<STYLE>
-A:link		{color: #a30001;
-		 text-decoration: underline;}
-A:visited	{color: #a30001;
-		 text-decoration: underline;}
-A:active	{color: #a30001;
-		 text-decoration: underline;}
-A:hover		{color: #df5348;
-		 text-decoration: underline;}
-td		{font-family: Verdana, Arial, Helvetica, sans-serif;
-		 font-size:8.8pt;}
-body		{font-family: Verdana, Arial, Helvetica, sans-serif;
-		 font-size:8pt;}
-.top		{color: #bfbfbf;
-		 font-size:8pt;}
-.top A:link	{color: #a30d01;
-		 text-decoration: none;}
-.top A:visited	{color: #a30d01;
-		 text-decoration: none;}
-.top A:active	{color: #a30d01;
-		 text-decoration: none;}
-.top A:hover	{color: #a30d01;
-		 text-decoration: underline;}
-.nav		{color:white;
-		 font-size:8pt;}
-.nav A:link	{color: white;
-		 text-decoration: none;}
-.nav A:visited	{color: white;
-		 text-decoration: none;}
-.nav A:active	{color: white;
-		 text-decoration: none;}
-.nav A:hover	{color: white;
-		 text-decoration: underline;}
-.lnav		{color:white;
-		 font-size:8pt;}
-.lnav A:link	{color: white;
-		 font-weight:bold;
-		 text-decoration: none;}
-.lnav A:visited	{color: white;
-		 font-weight:bold;
-		 text-decoration: none;}
-.lnav A:active	{color: white;
-		 font-weight:bold;
-		 text-decoration: none;}
-.lnav A:hover	{color: white;
-		 font-weight:bold;
-		 text-decoration: underline;}
-.lnav ul	{padding:0;
-		 margin-left:3pt;
-		 margin-right:0;
-		 list-style:square;
-		 padding-left: 1em;
-		 text-indent: -.2em;}
-.lnav li	{margin-top:.5em;}
-</STYLE>
-
-</head>
-
-<body>
-<center>
-<table width="910" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="55" valign="top"><img src="layout/logo_left.jpg"></td>
-<td width="800" valign="top">
-
-<!-- ####################### START HEADER INFO ######################-->
-<table width="800" cellpadding="0" cellspacing="0" border="0">
-<tr>
-<td width="133">
-<img src="layout/logo_top.jpg"><br>
-<a href ="?page=home"><img src="layout/logo_mid1.jpg"></a><br>
-<img src="layout/logo_mid2.jpg"><br>
-<img src="layout/logo_mid3.jpg"><br>
-</td>
-<td width="667" valign="top">
-<table width="667" cellpadding="0" cellspacing="0" border="0" bgcolor="white">
-<tr><td width="667" height="20" colspan="2"></td></tr>
-<tr><td width="261" height="28" background="layout/top_ncsu.jpg"></td><td width="406" height="28" align="center"><span class="top">
-
-<!-- #### START TOP LINKS #### -->
-<a href="?page=home">HOME</a> <font size="3">|</font>
-<a href="?page=about">ABOUT</a> <font size="3">|</font>
-<a href="?page=stu_app">APPLICATION</a> <font size="3">|</font>
-<a href="?page=spon_sponsorcamper">SPONSOR A CAMPER</a> <font size="3">|</font>
-<a href="?page=contact">CONTACT</a>
-<!-- ##### END TOP LINKS ##### -->
-
-</span></td></tr>
-<tr><td width="261" heigh="57" background="layout/top_sat.jpg"></td><a href ="?page=home"><td width="406" height="57" background="layout/top_abr.jpg"></a></td></tr>
-<tr><td width="667" height="3" colspan="2"></td></tr>
-<tr><td width="667" height="12" colspan="2" bgcolor="#171717"></td></tr>
-</table>
-</td>
-</tr>
-</table>
-<!-- ######################### END HEADER INFO #######################-->
-
-<!-- ######################################### -->
-<!-- ############ START NAVIGATION ########### -->
-<!-- ######################################### -->
-<table width="800" height="81" cellpadding="0" cellspacing="0" border="0" bgcolor="#666666">
-<tr>
-<td width="133" valign="top"><span class="nav">
-<img src="layout/logo_bot.jpg"><br>
-
-<!-- START NAV LEFT -->
-
-<font size="2"><b>
-&nbsp;&nbsp;&nbsp;<img src="layout/nav_lbul.jpg"><a href="?page=about">About Us</a><br>
-&nbsp;&nbsp;&nbsp;<img src="layout/nav_lbul.jpg"><a href="?page=alumni">Alumni</a>
-</b></font>
-
-<!-- END NAV LEFT -->
-
-</span></td>
-<td width="667" valign="top">
-<table cellpadding="0" cellspacing="0" border="0"><tr>
-<td width="167" valign="top"><span class="nav">
-
-<!-- START NAV MAIN -->
-
-<font size="2"><b>STUDENTS</b></font><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=stu_activities">Activities</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=stu_app">Application\<br>
-					&nbsp;&nbsp;&nbsp;&nbsp;Recommendations</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=stu_whattobring">What you need to bring</a>
-</span></td>
-<td width="167" valign="top"><span class="nav">
-
-
-<font size="2"><b>PARENTS</b></font><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=par_directions">Directions to NC State</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=par_samplesched">Sample Schedule</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=par_whotakescare">Who Takes Care of<br>
-					&nbsp;&nbsp;&nbsp; Your Child</a>
-</span></td>
-<td width="167" valign="top"><span class="nav">
-
-
-<font size="2"><b>TEACHERS</b></font><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=tch_recommendation">Recommendation Form</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=tch_students">Students We Target</a><br>
-</span></td>
-<td width="166" valign="top"><span class="nav">
-
-
-<font size="2"><b>SPONSORS</b></font><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=spon_current">Current Sponsors</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=spon_sponsorcamper">Sponsor A Camper</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=spon_counselors">Potential Counselors</a><br>
-<img src="layout/nav_mbul.jpg"><a href="?page=spon_report">Annual Report</a><br>
-
-<!-- END NAV MAIN -->
-
-
-</span></td>
-</tr></table>
-</tr>
-</table>
-
-<!-- ######################################### -->
-<!-- ############# END NAVIGATION #############-->
-<!-- ######################################### -->
-
-<table width="800" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="800" height="4"></td></tr>
-<tr><td width="800" height="3" bgcolor="#171717"></td></tr>
-<tr><td width="800" height="5"></td></tr>
-</table>
-
-<table width="800" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="245" valign="top">
-<table width="245" cellpadding="0" cellspacing="0" border="0" bgcolor="#e1e2da">
-<tr><td width="5" bgcolor="#e1e2da"></td><td width="235" valign="top">
-
-<!-- ## LEFT PANE ## -->
-
-<table width="235" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="235" height="5" colspan="3"></td></tr>
-<tr bgcolor="#d94d4d"><td width="5"></td><td width="225" height="30"><span class="nav"><font size="2"><b>
-
-Recent News</b></font></span></td><td width="5"></td></tr>
-<tr><td width="225"  height="3" bgcolor="#f36565" colspan="3"></td></tr>
-<tr bgcolor="#f36565"><td width="5"></td><td width="225"><span class="lnav">
-
-<?php include "nav_news.txt"; ?>
-
-</span></td><td width="5"></td></tr>
-<tr><td width="225"  height="3" bgcolor="#f36565" colspan="3"></td></tr>
-<tr><td width="235" height="5" colspan="3"></td></tr>
-<tr bgcolor="#d3a84b"><td width="5"></td><td width="225" height="30"><span class="nav"><font size="2"><b>
-
-Quick Links</b></font></span></td><td width="5"></td></tr>
-<tr><td width="225"  height="3" bgcolor="#f3c665" colspan="3"></td></tr>
-<tr bgcolor="#f3c665"><td width="5"></td><td width="225"><span class="lnav">
-
-<?php include "nav_quicklinks.txt"; ?>
-
-</span></td><td width="5"></td></tr>
-<tr><td width="225"  height="3" bgcolor="#f3c665" colspan="3"></td></tr>
-<tr><td width="235" height="5" colspan="3"></td></tr>
-<tr bgcolor="#658eb2"><td width="5"></td><td width="225" height="30"><span class="nav"><font size="2"><b>
-
-Caldwell Fellows Program</b></font></span></td><td width="5"></td></tr>
-<tr><td width="225"  height="3" bgcolor="#77a5cf" colspan="3"></td></tr>
-<tr bgcolor="#77a5cf"><td width="5"></td><td width="225"><span class="lnav">
-
-<?php include "nav_caldwellfellows.txt"; ?>
-
-</span></td><td width="5"></td></tr>
-<tr><td width="225"  height="3" bgcolor="#77a5cf" colspan="3"></td></tr>
-<tr><td width="235" height="5" colspan="3"></td></tr>
-</table>
-<!-- ## END L PANE ## -->
-
-
-</td><td width="5" bgcolor="#e1e2da"></td></tr></table>
-</td><td width="15"></td><td width="540" valign="top">
-
-<!-- ######################################### -->
-<!-- $$$$$$$$$$$$$$ BODY START $$$$$$$$$$$$$$$ -->
-<!-- ######################################### -->
-
-<table width="540" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="540">
-
-<?php
-
-if (!$_GET['page']) {
-	readfile('./home.txt');
-} else {
-	// Only allow txt files in the current directory by always taking the last
-	// part of a multi-part path.
-	$page = array_pop(explode('/', str_replace('\\', '/', $_GET['page'])));
-	if (file_exists("./$page.txt")) {
-		readfile("./$page.txt");
-	} else {
-		readfile('./not-found.txt');
-	}
-}
-
-?>
-
-<br><br>
-</td></tr>
-</table>
-
-<!-- ######################################### -->
-<!-- $$$$$$$$$$$$$$$ BODY END $$$$$$$$$$$$$$$$ -->
-<!-- ######################################### -->
-
-</td></tr>
-</table>
-<table width="800" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="800" height="8"></td></tr>
-<tr><td width="800" height="5" bgcolor="#c40000"></td></tr>
-<tr><td width="800" height="3"></td></tr>
-<tr><td width="800" height="33" bgcolor="#c40000" align="right" valign="bottom"><span class="top"><a href="#top"><font color="white" size="1">Going Up?</font></a>&nbsp;</td></tr>
-<tr><td width="800" height="3" bgcolor="#c40000"></td></tr>
-<tr><td width="800" height="5"></td></tr>
-</table>
-<table width="800" cellpadding="0" cellspacing="0" border="0">
-<tr><td width="400"><img src="layout/bot_logo.jpg"></td>
-<td width="400" align="right" valign="top"><span class="top"><font size="1" color="black">Got Questions? <a href="?page=contact"><b>Contact Us.</b></a></font></span></td></tr>
-</table>
-
-</td>
-<td width="55"></td>
-</tr>
-</table>
-</center>
-</body>
-
+<DOCTYPE! html>
+  <head>
+    <title>NCSU SATELLITE: Science & Technology Enriching Lifelong Leadership in Tomorrow's Endeavors</title>
+    <link rel="stylesheet" type="text/css" href="/assets/style.css" />
+  </head>
+  <body>
+    <div id="center">
+      <!-- #### START TOP LINKS #### -->
+      <!--<img src="layout/logo_top.jpg" />
+      <img src="layout/top_ncsu.jpg" />-->
+      
+      <div class="topnav inline">
+        <a href="?page=home">HOME</a> <font size="3">|</font>
+        <a href="?page=about">ABOUT</a> <font size="3">|</font>
+        <a href="?page=stu_app">APPLICATION</a> <font size="3">|</font>
+        <a href="?page=spon_sponsorcamper">SPONSOR A CAMPER</a> <font size="3">|</font>
+        <a href="?page=contact">CONTACT</a>
+      </div>
+      <!-- ##### END TOP LINKS ##### -->
+      <header class="head">
+        <img src="assets/logo.png" class="logo"/>
+        <h2 style="text-align: center;">Science & Technology Enriching Lifelong Leadership in Tomorrow's Endeavors</h2>
+        <!-- 
+        <img src="layout/logo_mid1.jpg" />
+        <img src="layout/top_sat.jpg" />
+        <img src="layout/top_abr.jpg" />
+           -->
+      </header>
+      <hr class="cell" color="#000" size="10">
+      <nav>
+        <section>
+          <ul class="navigation flex">
+            <li><a href="?page=about">About Us</a></li>
+            <li><a href="?page=alumni">Alumni</a></li>
+        </section>
+        <section>
+          <header>Students</header>
+          <ul class="navigation">
+            <li><a href="?page=stu_activities">Activities</a></li>
+            <li><a href="?page=stu_app">Application\<br> Recommendations</a></li>
+            <li><a href="?page=stu_whattobring">What you need to bring</a></li>
+          </ul>
+        </section>
+        <section>
+          <header>Parents</header>
+          <ul class="navigation">
+            <li><a href="?page=par_directions">Directions to NC State</a></li>
+            <li><a href="?page=par_samplesched">Sample Schedule</a></li>
+            <li><a href="?page=par_whotakescare">Who Takes Care of Your Child</a></li>
+          </ul>
+        </section>
+        <section>
+          <header>Teachers</header>
+          <ul class="navigation">
+            <li><a href="?page=tch_recommendation">Recommendation Form</a></li>
+            <li><a href="?page=tch_students">Students We Target</a></li>
+          </ul>
+        </section>
+        <section>
+          <header>Sponsors</header>
+          <ul class="navigation">
+            <li><a href="?page=spon_current">Current Sponsors</a></li>
+            <li><a href="?page=spon_sponsorcamper">Sponsor A Camper</a></li>
+            <li><a href="?page=spon_counselors">Potential Counselors</a></li>
+            <li><a href="?page=spon_report">Annual Report</a></li>
+          </ul>
+        </section>
+      </nav>
+      <hr class="black">
+      <aside>
+        <section class="red">
+          <header>Recent News</header>
+          <?php include "nav_news.txt"; ?>
+        </section>
+        <section class="yellow">
+          <header>Quick Links</header>
+          <?php include "nav_quicklinks.txt"; ?>
+        </section>
+        <section class="blue">
+          <header>Caldwell Fellows Program</header>
+          <?php include "nav_caldwellfellows.txt"; ?>
+        </section>
+      </aside>
+      <article class="main">
+        <?php
+        if (!$_GET['page']) {
+	          readfile('./home.txt');
+        } else {
+	          // Only allow txt files in the current directory by always taking the last
+	          // part of a multi-part path.
+	          $page = array_pop(explode('/', str_replace('\\', '/', $_GET['page'])));
+	          if (file_exists("./$page.txt")) {
+		            readfile("./$page.txt");
+	          } else {
+		            readfile('./not-found.txt');
+	          }
+        }
+        ?>
+      </article>
+      <footer>
+        <hr color="#c40000">
+        <hr color="#c40000" size="30">
+        <img src="layout/bot_logo.jpg" />
+        <span class="right"><font size="1" color="black">Got Questions? <a href="?page=contact"><b>Contact Us.</b></a></font></span>
+      </footer>
+    </div>
+  </body>
 </html>
